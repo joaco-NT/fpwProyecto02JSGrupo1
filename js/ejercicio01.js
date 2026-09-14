cffc8fe1fa5299d36623942437af259ea226e841
joaco-NT <joacontorrez@gmail.com>
2026-09-14 11:16:58 -0300

    TorrezJoaquin - Ejercicio1e

--- js/ejercicio01.js
+++ js/ejercicio01.js
@@ -1,4 +1,16 @@
 const ejecutarEjercicio01 = () => {
-    // Tu código para el ejercicio 1 aquí...
-    alert("¡Aquí se ejecutará el Ejercicio 1!");
+    let letra1 = prompt("Ingresa la primera letra:");
+    let letra2 = prompt("Ingresa la segunda letra:");
+
+    if (letra1 && letra2) {
+        if (letra1 < letra2) {
+            alert(`La letra "${letra1}" está ubicada antes que "${letra2}".`);
+        } else if (letra1 > letra2) {
+            alert(`La letra "${letra1}" está ubicada después de "${letra2}".`);
+        } else {
+            alert(`Ambas letras son idénticas ("${letra1}").`);
+        }
+    } else {
+        alert("Por favor, ingresa un valor válido para ambas letras.");
+    }
 };
